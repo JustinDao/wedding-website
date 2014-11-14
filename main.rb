42df32e22294c21047ae96ad6a5733899fe4065d
Justin <tjd5at@virginia.edu>
2014-11-14 10:38:15 -0500

    Adds global password login funcitonality

--- main.rb
+++ main.rb
@@ -1,5 +1,43 @@
 require 'sinatra'
-require 'haml'
+
+# https://stackoverflow.com/questions/26896375
+use Rack::Session::Pool 
+
+helpers do
+  def logged_in?
+    session["is_logged_in"] == true
+  end
+end
+
+before do
+  pass if request.path_info.split('/')[1] == 'login'
+
+  if not logged_in?
+    redirect '/login'
+  end
+end
+
+get "/login" do
+  if logged_in?
+    redirect '/'
+  else
+    erb :login
+  end
+end
+
+post '/login' do
+  if params['password'] == "mypassword"
+    session["is_logged_in"] = true
+    redirect '/'
+  else
+    halt 401
+  end
+end
+
+get '/logout' do
+  session["is_logged_in"] = false 
+  redirect '/'
+end
 
 get '/' do
   erb :index
