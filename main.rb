a46649fffe96643448ad2761636cddab2806ef7b
Justin Dao <tjd5at@virginia.edu>
2014-12-14 21:35:02 -0500

    Set expire time to 30 days, adds hashed password to login check

--- main.rb
+++ main.rb
@@ -1,14 +1,15 @@
 require 'sinatra'
 require 'config_env'
+require 'digest'
 
 ConfigEnv.path_to_config("#{__dir__}/config/config_env.rb")
 
 # https://stackoverflow.com/questions/26896375
-use Rack::Session::Pool 
+use Rack::Session::Pool, :expire_after => 2592000
 
 helpers do
   def logged_in?
-    session["is_logged_in"] == true
+    session["is_logged_in"] && session["password"] == Digest::MD5.hexdigest(ENV['login_password'])
   end
 end
 
@@ -16,7 +17,11 @@ before do
   pass if request.path_info.split('/')[1] == 'login'
 
   if not logged_in?
+    session["password"] = nil
+    session["is_logged_in"] = false
     redirect '/login'
+  else
+    session["is_logged_in"] = true
   end
 end
 
@@ -31,6 +36,7 @@ end
 post '/login' do
   if params['password'] == ENV['login_password']
     session["is_logged_in"] = true
+    session["password"] = Digest::MD5.hexdigest ENV['login_password']
     redirect '/'
   else
     halt 401
@@ -39,6 +45,7 @@ end
 
 get '/logout' do
   session["is_logged_in"] = false 
+  session["password"] = nil
   redirect '/'
 end
 
