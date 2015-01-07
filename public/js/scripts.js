2a9864c1e635dde27438f7984fe936b749c5deac
Justin Dao <tjd5at@virginia.edu>
2015-01-07 12:03:21 -0500

    Initial site redesign

---
+++ public/js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  $.stellar({
+    horizontalScrolling: false,
+    responsive: true,
+  });
+
+  function initialize() {
+    var mapCanvas = document.getElementById('map_canvas');
+
+    var mapOptions = {
+      center: new google.maps.LatLng(38.062358, -78.728313),
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(38.062358, -78.728313),
+      map: map
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
