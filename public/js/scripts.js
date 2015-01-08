a57300ca4c3b8f774a2df878fa48a7c457f12bff
Justin Dao <tjd5at@virginia.edu>
2015-01-07 19:58:16 -0500

    Initial photoswipe set up

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,38 +5,51 @@ $(document).ready(function() {
     responsive: true,
   });
 
+  var pswpElement = document.querySelectorAll('.pswp')[0];
+
+  $(".gallery-link").click(function(e) {
+    e.preventDefault();
+
+    var thisImage = $(this);
+    var index = -1;
+    var items = [];
+
+    $(".gallery-link").each(function(i, val) {
+      // http://stackoverflow.com/questions/2639070/get-the-full-uri-from-the-href-property-of-a-link
+      if (val.href == thisImage.get(0).href) {
+        index = i;
+      }
+
+      //http://stackoverflow.com/questions/8636857/get-the-width-of-an-image-specified-by-an-url-jquery
+      var tmpImg = new Image();
+      tmpImg.src = val.href;
+
+      var width = val.dataset.width;
+      var height = val.dataset.height;
+
+      items.push({
+        src: val.href,
+        w: width,
+        h: height,
+      });      
+    });
+   
+    // define options (if needed)
+    var options = {
+        index: index
+    };
+
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+
+  });
+
   // $(".jump-anchor").click(function(e) {
   //   e.preventDefault();
   //   console.log($(this).attr('href'));
   //   $('body').scrollTo($(this).attr('href'));
-  // });
-
-  // var pswpElement = document.querySelectorAll('.pswp')[0];
-
-  // // build items array
-  // var items = [
-  //     {
-  //         src: 'https://placekitten.com/600/405',
-  //         w: 600,
-  //         h: 400
-  //     },
-  //     {
-  //         src: 'https://placekitten.com/1200/900',
-  //         w: 1200,
-  //         h: 900
-  //     }
-  // ];
-
-  // // define options (if needed)
-  // var options = {
-  //     // optionName: 'option value'
-  //     // for example:
-  //     index: 0 // start at first slide
-  // };
-
-  // // Initializes and opens PhotoSwipe
-  // var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-  // gallery.init();
+  // }); 
 
   function initialize() {
     var mapCanvas = document.getElementById('map_canvas');
